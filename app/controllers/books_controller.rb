12dc23bb29b79775c31aece5bcc1b1dba827fb84
EC2 Default User <ec2-user@ip-172-31-61-237.ec2.internal>
2021-03-17 09:44:59 +0000

    Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,22 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     # １. データを新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+    @book.save
+    # サクセスメッセージ
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+
     # ３. トップ画面へリダイレクト
-    redirect_to '/show'
+    redirect_to book_path(@book.id)
+
+    else
+      render "index"
+    end
+
   end
 
   def show
@@ -24,13 +34,29 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+
+
+  # サクセスメッセージ
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book)
+    else
+      render 'edit'
+
+    end
+
   end
 
   def destroy
     @book = Book.find(params[:id])
-    @book.destroy
+    
+    
+  # サクセスメッセージ
+    if @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
+    end
+
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+
+  # バリデーションチェック
+  validates :title, :body, presence: true
+
 end
